8c9fffec5984ed3bffdd19b74aa042c83a64dd1a
Jonathan McCoy <modelsolutions@gmail.com>
2015-10-08 07:35:14 -0700

    testPath test method

--- server/users.js
+++ server/users.js
@@ -1,3 +1,9 @@
 Meteor.publish("users", function () {
   return Meteor.users.find({}, {fields: {emails: 1, profile: 1}});
 });
+
+Meteor.methods({
+  testPath: function() {
+    console.log(path);
+  }
+});
